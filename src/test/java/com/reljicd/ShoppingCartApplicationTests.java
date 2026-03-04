aa475ac236cf527ebc6a6155c6b9dd29a5d7dc97
d ι n e s h κ u m α r <70446268+dinesh-official@users.noreply.github.com>
2026-03-04 19:06:38 +0530

    Update ShoppingCartApplicationTests.java

--- src/test/java/com/reljicd/ShoppingCartApplicationTests.java
+++ src/test/java/com/reljicd/ShoppingCartApplicationTests.java
@@ -1,16 +1,10 @@
-package com.reljicd;
+import org.springframework.test.context.ActiveProfiles;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShoppingCartApplicationTests {
+@ActiveProfiles("test")
+class ShoppingCartApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
